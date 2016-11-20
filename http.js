b804e9d1766b1825daeef375615392958727bb81
Suo Qin <kpsesuo@gmail.com>
2016-11-20 18:54:56 +1100

    return text/html response

--- http.js
+++ http.js
@@ -4,8 +4,13 @@ const http = require('http')
 const server = http.createServer()
 
 server.on('request', (req, res) => {
-  res.writeHead(200, {"Content-Type": 'text/plain'})
-  res.end("server is an events emmiter, 'request' is a event name.")
+  if (req.url == '/') {
+    res.writeHead(200, {"Content-Type": 'text/html'})
+    res.end("server is an <strong>events emmiter</strong>, 'request' is a event name.")
+  } else if (req.url == '/goodbye') {
+    res.writeHead(200, {"Content-Type": 'text/html'})
+    res.end("goodbye <strong>world</strong>")
+  }
 })
 
 server.listen(3000, ()=> {
